7a1c2c16c1e53e35e6b007637c18255c8dddca52
AadiVasa1 <aadivasa@utexas.edu>
2025-07-31 16:02:40 -0700

    Update CellStaggeredSpacingRunner.py

--- CellStaggeredSpacingRunner.py
+++ CellStaggeredSpacingRunner.py
@@ -22,14 +22,18 @@ h=[]
 for j in SL:  # Outer loop for rows
     row = []  # Create an empty row
     for i in ST:  # Inner loop for columns
-        row.append(c.known_v_and_spacing(2,i,j)/c.known_v_and_spacing(2,i,j, True))  # Append an initial value (e.g., 0) to the row
+        v, p = c.known_h_and_spacing(50, i, j)
+        row.append(1/(v*p))  # Append an initial value (e.g., 0) to the row
+
+        # hh, p = c.known_v_and_spacing(2, i, j)
+        # row.append(hh)
     h.append(row)  # Append the filled row to the main array
 #print(h)
 Z=np.array(h)
 
 surf = ax.plot_surface(X, Y, Z,  cmap = plt.cm.cividis)
 
-ax.scatter(.028,.022,22692, color = "red", s=50)
+# ax.scatter(.028,.022,22692, color = "red", s=50)
 
 ax.set_xlabel('Transverse Pitch (m)', labelpad=20)
 ax.set_ylabel('Longitudinal Pitch (m)', labelpad=20)
@@ -38,5 +42,20 @@ ax.set_title('Cell Spacing vs Convection Coefficient')
 
 plt.show()
 
-c.known_v_optimize_spacing(2,[.022, .04], [.022, .04], minimum_h=100)
-print(c.known_v_and_spacing(2,.028, .022))
+# for i in range(15):
+#     c.known_v_optimize_spacing(i+1,[.022, .07], [.022, .07])
+#     print(i)
+#     print()
+
+for i in range(99):
+    print(i*5)
+    c.known_h_optimize_spacing(5*(i+1), [.022, .07], [.022, .07])
+    print()
+# print(c.known_v_and_spacing(20,.028, .022))
+# print(c.known_v_and_spacing(20,.028, .022))
+# print()
+# print(c.known_h_and_spacing(410, .028, .022))
+# print(c.known_h_and_spacing(410, .028, .022)
+# c.known_h_optimize_spacing(50,[.022, .04], [.020, .04])
+# print(c.known_h_and_spacing(50, .022, .020)) #(0.11450290661719202, 0.0005691083508591201)
+# print(c.known_v_and_spacing(.11817839031716551, .022,.022))
