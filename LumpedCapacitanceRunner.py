ddff869421207ae6820f2f109d758c614b2f80be
Aadi Vasa <aadivasa@utexas.edu>
2025-07-31 22:46:10 -0700

    updated desired temp rise in runner

--- LumpedCapacitanceRunner.py
+++ LumpedCapacitanceRunner.py
@@ -3,7 +3,7 @@
 import LumpedCapacitance as lc
 import matplotlib.pyplot as plt
 
-s = lc.LumpedCapacitanceModel(70, 128, 4, .02155, .07015, .07, .015, 790, 33, 43)
+s = lc.LumpedCapacitanceModel(70, 128, 4, .02155, .07015, .07, .015, 790, 33, 38)
 
 t, temp = s.temperature_over_time(avg_convection_coefficient = 44, runtime = 3600)
 
