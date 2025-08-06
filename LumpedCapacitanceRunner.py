cb4dd7aa2060df0dea7d8b593c6c3b679c4dcce5
AadiVasa1 <aadivasa@utexas.edu>
2025-08-06 14:46:47 -0700

    Update LumpedCapacitanceRunner.py

--- LumpedCapacitanceRunner.py
+++ LumpedCapacitanceRunner.py
@@ -3,12 +3,14 @@
 import LumpedCapacitance as lc
 import matplotlib.pyplot as plt
 
-s = lc.LumpedCapacitanceModel(70, 140, 4, .0186, .0652, .047, .017, 790, 33, 38)
+s = lc.LumpedCapacitanceModel(70, 140/5, 4, .0186, .0652, .047, .017, 790, 33, 50)
 
-t, temp = s.temperature_over_time(avg_convection_coefficient = 56, runtime = 3600)
+t, temp = s.temperature_over_time(avg_convection_coefficient = 101, runtime = 3600)
 
 print(s.required_convection_coefficient(60))
-print(s.biot_number(.02155, 35, 1))
+print(s.biot_number(.0186, 101, 5.1))
+area_inlet = .120*.120
+print(s.required_volumetric_flow_rate()/area_inlet)
 print(s.required_volumetric_flow_rate())
 
 plt.plot(t, temp)
