074d8e494c0c5439e72a6114a61e649f7b20f0e4
Aadi Vasa <aadivasa@utexas.edu>
2025-08-06 20:00:21 -0700

    rms added

--- CellStaggeredSpacingRunner.py
+++ CellStaggeredSpacingRunner.py
@@ -2,20 +2,19 @@ import CellStaggeredSpacing as cs
 import matplotlib.pyplot as plt
 import numpy as np
 
-c = cs.CellStaggeredSpacing(14, 18.6e-3)
-# print(c.known_v_and_spacing(6,.0313,.0343))
-# print(c.known_v_and_spacing(6,.0313,.0343, True))
-#
+c = cs.CellStaggeredSpacing(24, 18.6e-3)
+
+
 ST=[] #Transverse Pitch
 SL=[] #Longitudinal Pitch
 x = 0.019
 while x < 0.025:
     ST.append(x)
-    x = x + 0.0001
+    x = x + 0.001
 x = 0.0174
 while x < .025:
     SL.append(x)
-    x = x + 0.0001
+    x = x + 0.001
 
 fig = plt.figure(figsize = (10,10))
 ax = plt.axes(projection='3d')
@@ -28,7 +27,7 @@ for j in SL:  # Outer loop for rows
         # v, p = c.known_h_and_spacing(73, i, j)
         # row.append(v)  # Append an initial value (e.g., 0) to the row
 
-        hh, p = c.known_v_and_spacing(1.93, i, j)
+        hh, p = c.known_v_and_spacing(2.189, i, j)
         row.append(hh/p)
     h.append(row)  # Append the filled row to the main array
 #print(h)
@@ -55,9 +54,9 @@ plt.show()
 #     c.known_h_optimize_spacing(5*(i+1), [.022, .07], [.022, .07])
 #     print()
 
-t, l, _ = c.known_v_optimize_spacing(1.93, [.02, .025], [.0174, .025], 74, .0001, .0001)
-print(c.known_v_and_spacing(1.93, t, l))
-print(c._v_max(.0186, .02, .0174, 1.93))
+t, l, _ = c.known_v_optimize_spacing(2.819, [.02, .025], [.0174, .025], 62.11, .0001, .0001)
+print(c.known_v_and_spacing(2.819, t, l))
+print(c._v_max(.0186, .02, .0174, 2.819))
 
 
 # c.known_h_optimize_spacing(56,[.02, .03], [.0174, .03], .0001, .0001)
--- LumpedCapacitanceRunner.py
+++ LumpedCapacitanceRunner.py
@@ -3,15 +3,17 @@
 import LumpedCapacitance as lc
 import matplotlib.pyplot as plt
 
-s = lc.LumpedCapacitanceModel(70, 140/5, 4, .0186, .0652, .047, .017, 790, 33, 50)
+s = lc.LumpedCapacitanceModel(70, 24, 4, .0186, .0652, .047, .017, 790, 33, 43)
 
 t, temp = s.temperature_over_time(avg_convection_coefficient = 101, runtime = 3600)
 
-print(s.required_convection_coefficient(60))
-print(s.biot_number(.0186, 101, 5.1))
+print(f"h: {s.required_convection_coefficient(60)}")
+# print(s.biot_number(.0186, 101, 5.1))
+
 area_inlet = .120*.120
-print(s.required_volumetric_flow_rate()/area_inlet)
-print(s.required_volumetric_flow_rate())
+
+print(f"v: {s.required_volumetric_flow_rate()/area_inlet}")
+print(f"q: {s.required_volumetric_flow_rate()}")
 
 plt.plot(t, temp)
 plt.show()
